5b8b06d78b827a39734ab4a969093fd150ed8a22
Mina <mina.selim2005@hotmail.com>
2019-04-05 15:35:43 -0400

    Task 2

--- src/Monitor.java
+++ src/Monitor.java
@@ -12,13 +12,21 @@ public class Monitor
 	 * ------------
 	 */
 
-
+	private int numberOfPhilosophers;
+	private boolean chopstickAvailable[];
+	private boolean talking;
 	/**
 	 * Constructor
 	 */
 	public Monitor(int piNumberOfPhilosophers)
 	{
-		// TODO: set appropriate number of chopsticks based on the # of philosophers
+		numberOfPhilosophers = piNumberOfPhilosophers;
+		talking = false;
+		chopstickAvailable = new boolean[piNumberOfPhilosophers];
+		for(int i = 0; i<chopstickAvailable.length ; i++)
+		{
+			chopstickAvailable[i] = true;
+		}
 	}
 
 	/*
@@ -33,7 +41,19 @@ public class Monitor
 	 */
 	public synchronized void pickUp(final int piTID)
 	{
-		// ...
+		while(!canPickup(piTID))
+		{
+			try {
+				System.out.println("Philosopher " + piTID + " is waiting to eat");
+				this.wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		chopstickAvailable[getLeftChopStick(piTID)] = false;
+		chopstickAvailable[getRightChopStick(piTID)] = false;
 	}
 
 	/**
@@ -42,7 +62,9 @@ public class Monitor
 	 */
 	public synchronized void putDown(final int piTID)
 	{
-		// ...
+		chopstickAvailable[getLeftChopStick(piTID)] = true;
+		chopstickAvailable[getRightChopStick(piTID)] = true;
+		this.notifyAll();
 	}
 
 	/**
@@ -51,7 +73,16 @@ public class Monitor
 	 */
 	public synchronized void requestTalk()
 	{
-		// ...
+		while(talking)
+		{
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		talking = true;
 	}
 
 	/**
@@ -60,7 +91,36 @@ public class Monitor
 	 */
 	public synchronized void endTalk()
 	{
-		// ...
+		talking = false;
+		notifyAll();
+	}
+	
+	private synchronized boolean canPickup(final int piTID)
+	{
+		return leftChopStickAvailable(piTID) && rightChopStickAvailable(piTID);
+	}
+	
+	private synchronized boolean leftChopStickAvailable(final int piTID)
+	{
+		int leftChopstick = piTID != 0 ? piTID - 1 : numberOfPhilosophers-1;
+		return chopstickAvailable[leftChopstick];
+		
+	}
+	
+	private synchronized boolean rightChopStickAvailable(final int piTID)
+	{
+		int rightChopstick = piTID != numberOfPhilosophers ? piTID : 0;
+		return chopstickAvailable[rightChopstick];
+	}
+	
+	private synchronized int getLeftChopStick(final int piTID)
+	{
+		return piTID != 0 ? piTID - 1 : numberOfPhilosophers-1;		
+	}
+	
+	private synchronized int getRightChopStick(final int piTID)
+	{
+		return piTID != numberOfPhilosophers ? piTID : 0;
 	}
 }
 
